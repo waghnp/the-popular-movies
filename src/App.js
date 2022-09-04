a1aa77f9171317f51bcb7585f482465f14bf4b80
Nilesh Wagh <waghn650@gmail.com>
2022-09-04 17:32:17 +0530

    favourites page and routing complete

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import './App.css';
 import Banner from './Components/Banner';
+import Favourites from './Components/Favourites';
 import Movies from './Components/Movies';
 import Navbar from './Components/Navbar';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 
 function App() {
   return (
-    <>
+    <Router>
       <Navbar/>
-      <Banner/>
-      <Movies/>
-    </>
+      <Routes>
+        <Route path='/' element={<><Banner/><Movies/></>}/>
+        <Route path='/favourites' element={<Favourites/>}/>
+      </Routes>
+    </Router>
   );
 }
 
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react'
-
+import {Link} from 'react-router-dom';
 export default class Navbar extends Component {
   render() {
     return (
       <div style={{display:'flex',padding:'15px'}}>
-        <h1>The Movies</h1>
-        <h2 style={{marginLeft:'25px',marginTop:'10px'}}>Favourites</h2>
+       <Link to='/'> <h1>The Movies</h1> </Link>
+        <Link to='/favourites'><h2 style={{marginLeft:'25px',marginTop:'7px'}}>Favourites</h2></Link>
       </div>
     )
   }
